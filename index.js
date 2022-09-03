825247415d65582533a65e4ba95b64fa2423cf6e
akram hoque <oriniusakram511@gmail.com>
2022-09-03 14:32:13 +0600

    added comment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId  } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000 ;
@@ -7,15 +8,46 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// verify jwt
 
+function verifyJWT (req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: "unauthorized access"})
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(error, decoded) =>{
+    if(error){
+      return res.status(403).send({message: 'Forbidden access'})
+    }
+    console.log('show decoded', decoded)
+    req.decoded = decoded ;
+  })
+      // console.log('verify jwt' , authHeader);
+      next();
+}
 
-const uri = "mongodb+srv://dbUser3:LOHAcm0u3JUrLhgc@cluster0.b3o9khw.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b3o9khw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
 
     try{
         await client.connect();
         const fruitCollection = client.db("wareHouse").collection("items"); 
+        const newOrderCollection = client.db('fruitHouse').collection('order');
+
+
+        // AUTH
+        app.post('/token', async(req, res) => {
+          const email = req.body;
+          console.log(email);
+          const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET , {
+              expiresIn: '1d'
+          });
+          res.send({ accessToken });
+          console.log(accessToken)
+      })
+
 
         app.get('/items', async(req, res) => {
         const query = {};
@@ -23,6 +55,18 @@ async function run(){
         const items = await cursor.toArray(); 
         res.send(items);
         });
+
+
+        app.post('/items', async(req, res) => {
+          const items = req.body ;
+          console.log(items);
+          const result = await fruitCollection.insertOne(items);
+          res.send(result);
+        } );
+    
+
+
+
 
 
         app.get('/inventory/:id', async(req, res) =>{
@@ -80,6 +124,31 @@ app.post('/order', async (req, res) => {
   res.send(result);
   });
     
+  // test heroku
+
+  // myItem
+
+
+
+ // create order from database and show in ui
+ app.get('/order', async(req, res) =>{
+  const email = req.query.email ;
+  
+      // console.log(email)
+  const query = {email:email};
+  const cursor = newOrderCollection.find(query);
+  const orders = await cursor.toArray();
+  res.send(orders);
+
+  
+})
+
+app.delete('/order/:id' , async(req, res) =>{
+  const id = req.params.id ;
+  const query = {_id: ObjectId(id)} ;
+  const result = await newOrderCollection.deleteOne(query) ;
+  res.send(result);
+});
  
 
 
